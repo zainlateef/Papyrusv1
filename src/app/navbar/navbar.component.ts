d3daa0577b1e69893e3e35e2aa6a1197c2c69b6b
Zain Lateef <zainlateef@Zains-MacBook-Pro.local>
2018-06-03 18:27:56 -0400

    testing

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  template: 
+  `<nav class="nav">
+      <div class="burger" [ngClass]="{'burger--active':showMenu}" (click)="toggleMenu()">
+        <div class="burger__patty"></div>
+      </div>
+
+      <ul class="nav__list" [ngClass]="{'nav__list--active':showMenu}">
+        <li class="nav__item">
+          <a href="#1" class="nav__link c-blue"><i class="fa fa-camera-retro"></i></a>
+        </li>
+        <li class="nav__item">
+          <a href="#2" class="nav__link c-yellow scrolly"><i class="fa fa-bolt"></i></a>
+        </li>
+        <li class="nav__item">
+          <a href="#3" class="nav__link c-red"><i class="fa fa-music"></i></a>
+        </li>
+        <li class="nav__item">
+          <a href="#4" class="nav__link c-green"><i class="fa fa-paper-plane"></i></a>
+        </li>
+      </ul>
+
+    </nav>`,
+  styleUrls: ['./navbar.component.css','./original_style.scss']
+})
+export class NavbarComponent
+{
+    
+    showMenu : boolean = false;
+    toggleMenu()
+    {
+      this.showMenu=!this.showMenu;
+    }
+}
