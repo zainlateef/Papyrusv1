f7d7c322c10a44fe73e783a51728df59ed55f761
Zain Lateef <zainlateef@Zains-MacBook-Pro.local>
2018-06-09 19:41:29 -0400

    move on. close the book on unit testing. test on your own

--- src/app/Pages/user-page/feed/feed.component.spec.ts
+++ src/app/Pages/user-page/feed/feed.component.spec.ts
@@ -1,27 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, tick, fakeAsync, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFontAwesomeComponent, AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FeedComponent } from './feed.component';
-import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from '../../../app.component';
+import { HomeComponent } from '../../home/home.component';
+import { MainFeedComponent } from '../../main-feed/main-feed.component';
+import { UserPageComponent } from '../user-page.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { InteractComponent } from '../../interact/interact.component';
+import { PublishComponent } from '../../publish/publish.component';
+import { ViewPostComponent } from '../../view-post/view-post.component';
+import { ErrorPageComponent } from '../../error-page/error-page.component';
+import { NavItemComponent } from '../navbar/nav-item/nav-item.component';
+import { routes } from '../../../app.module';
+import { Router } from '@angular/router';
+
 
 describe('FeedComponent', () => {
-  let component: FeedComponent;
-  let fixture: ComponentFixture<FeedComponent>;
+
+  let component : FeedComponent;
+  let fixture : ComponentFixture<FeedComponent>;
+  let router : Router;
+  let location : Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FeedComponent ],
-      imports: [ RouterTestingModule ]
-    })
-    .compileComponents();
-  }));
+      declarations: [
+        AppComponent,
+        HomeComponent,
+        MainFeedComponent,
+        UserPageComponent,
+        NavbarComponent,
+        FeedComponent,
+        InteractComponent,
+        PublishComponent,
+        ViewPostComponent,
+        ErrorPageComponent,
+        NavItemComponent
+      ],
+        imports: [RouterTestingModule.withRoutes(routes),AngularFontAwesomeModule],
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(FeedComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    router.initialNavigation();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  }));
+
+  it( 'recives the correct url parameters', fakeAsync(() => {
+    router.navigate(['feed']);
+    tick(50);
+    expect(component.loadOnUrlChange).toHaveBeenCalled();
+  }));
+
 });
