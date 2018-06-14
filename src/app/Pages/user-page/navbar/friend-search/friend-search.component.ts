42683f7d182ee4a2974238ff86a7c0f0a250321b
Zain Lateef <zainlateef@Zains-MacBook-Pro.local>
2018-06-14 12:37:03 -0400

    voila. It's beautiful. Proud of you baby. On to the menu

--- src/app/Pages/user-page/navbar/friend-search/friend-search.component.ts
+++ src/app/Pages/user-page/navbar/friend-search/friend-search.component.ts
@@ -83,7 +83,7 @@ export class FriendSearchComponent implements OnInit,OnDestroy {
     let user1=new User();
     let user2=new User();
     let user3=new User();
-    user1.uid="schmurd", user1.firstName="Bob",user1.lastName="Schmurda",user1.profilePic="https://media.gq.com/photos/57360cf4a522201d4b35bec2/16:9/w_1280/bobby-shmurda-football-gq-0616-01.jpg";
+    user1.uid="schmurd", user1.firstName="Bob",user1.lastName="5678901234567890123456789",user1.profilePic="https://media.gq.com/photos/57360cf4a522201d4b35bec2/16:9/w_1280/bobby-shmurda-football-gq-0616-01.jpg";
     user2.uid="jdirt", user2.firstName="Joe",user2.lastName="Dirt",user2.profilePic="https://resizing.flixster.com/pBPkXbpgQN3BbBwYVk4HFElqPfQ=/300x300/v1.aDs0ODg2O2o7MTc3MzA7MTIwMDs3MDA7NDYw";
     user3.uid="elBig", user3.firstName="Big",user3.lastName="L",user3.profilePic="https://img.discogs.com/sRJ6CCEJvkdRKadMMrXY8U4DMB8=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-39956-1499748788-5883.jpeg.jpg";
     this.userDatabase.push(user1,user2,user3);
