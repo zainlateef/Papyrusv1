856ef9be0f99cdda4d68ccfbf3892218fbcd02f6
Zain Lateef <zainlateef@Zains-MacBook-Pro.local>
2018-06-24 18:50:56 -0400

    this is the bandaged version. ill try to make it better

--- src/app/Pages/user-page/navbar/friend-search/friend-search.component.ts
+++ src/app/Pages/user-page/navbar/friend-search/friend-search.component.ts
@@ -9,9 +9,9 @@ import { User } from '../../../../Models/user';
 @Component({
   selector: 'friend-search',
   template: `
-  <div class="wrapper" (clickOutside)="onClickedOutside($event)">
-    <fa (click)="toggleSearch()" class="icon" name="search" size="2x"></fa>
-    <input [ngClass]="{'searchbar-closed' : !showSearchBar }" class="searchbar-open" type="search" [formControl]="search">
+  <div class="wrapper">
+    <fa (click)="toggleSearch()" [ngStyle]="{ 'opacity' : showSearchBar ? '0' : '1' }" class="icon" name="search" size="2x"></fa>
+    <input [ngClass]="{'searchbar-closed' : !showSearchBar }" class="searchbar-open" type="search" [formControl]="search" (clickOutside)="onClickedOutside($event)">
     <ul>
       <li [@zoomIn]="zoomIn" [routerLink]="['/feed',user.uid]" *ngFor="let user of userMatches">
       <div class="wrapper">
@@ -39,6 +39,7 @@ export class FriendSearchComponent implements OnInit,OnDestroy {
   subscription : any;
   userMatches : Set<User> = new Set;
   userDatabase : Array<User> = new Array;
+  counter : number = 0;
 
   ngOnInit()
   {
@@ -88,15 +89,23 @@ export class FriendSearchComponent implements OnInit,OnDestroy {
     let user1=new User();
     let user2=new User();
     let user3=new User();
-    user1.uid="schmurd", user1.firstName="Bob",user1.lastName="5678901234567890123456789",user1.profilePic="https://media.gq.com/photos/57360cf4a522201d4b35bec2/16:9/w_1280/bobby-shmurda-football-gq-0616-01.jpg";
+    user1.uid="schmurd", user1.firstName="Bob",user1.lastName="Antetekuompo",user1.profilePic="https://media.gq.com/photos/57360cf4a522201d4b35bec2/16:9/w_1280/bobby-shmurda-football-gq-0616-01.jpg";
     user2.uid="jdirt", user2.firstName="Joe",user2.lastName="Dirt",user2.profilePic="https://resizing.flixster.com/pBPkXbpgQN3BbBwYVk4HFElqPfQ=/300x300/v1.aDs0ODg2O2o7MTc3MzA7MTIwMDs3MDA7NDYw";
     user3.uid="elBig", user3.firstName="Big",user3.lastName="L",user3.profilePic="https://img.discogs.com/sRJ6CCEJvkdRKadMMrXY8U4DMB8=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-39956-1499748788-5883.jpeg.jpg";
     this.userDatabase.push(user1,user2,user3);
   }
 
   onClickedOutside(e: Event) {
-    this.showSearchBar=false;
-    this.userMatches.clear();
+    if(this.showSearchBar)
+    {
+      ++this.counter;
+      if(this.counter > 1)
+      {
+        this.showSearchBar=false;
+        this.userMatches.clear();
+        this.counter=0;
+      }
+    }
   }
 
   ngOnDestroy() 
