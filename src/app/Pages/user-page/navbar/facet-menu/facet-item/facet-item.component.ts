c0c206bf8bc3c8eb902e36d97f0790f687238353
Zain Lateef <zainlateef@Zains-MacBook-Pro.local>
2018-07-08 02:08:21 -0700

    got some toggling action

--- src/app/Pages/user-page/navbar/facet-menu/facet-item/facet-item.component.ts
+++ src/app/Pages/user-page/navbar/facet-menu/facet-item/facet-item.component.ts
@@ -7,11 +7,11 @@ import { EditButtonService } from '../../../../../Services/edit-button.service';
   template: 
   ` <a [routerLink]="editMode ? null : [{facet : item.facet}]" class="nav__link">
       <div class="wrapper">
-        <i [class]="item.iconName" [ngClass]="{'faa-float animated faa-fast': editMode}" [ngStyle]="{'color':item.color}"></i>
-        <div class="editMenu" *ngIf="editMode">
+        <i (click)="editMode ? toggleEditMenu() : null " [class]="item.iconName" [ngClass]="{'faa-float animated faa-fast': editMode}" [ngStyle]="{'color':item.color}"></i>
+        <div class="editMenu" *ngIf="editMode" (clickOutside)=" editMenu ? onClickedOutside($event) : null">
           <img src="/assets/images/close.png">
-          <input>
-          <button class="colorpicker" [(colorPicker)]="color" [style.background]="color" [cpPosition]="'bottom'" [cpDisableInput]="true"></button>
+          <input *ngIf="editMenu">
+          <button *ngIf="editMenu" class="colorpicker" [(colorPicker)]="color" [style.background]="color" [cpPosition]="'bottom'" [cpDisableInput]="true"></button>
         </div>
       </div>
     </a>
@@ -23,12 +23,31 @@ export class FacetItemComponent implements OnInit {
 
   @Input("item") item : FacetItem;
   editMode : boolean = false;
+  editMenu : boolean = false;
+  counter : number = 0;
   
   constructor(private editService : EditButtonService) {
   }
 
   ngOnInit() {
     this.editService.change.subscribe( editButtonEvent => this.editMode=editButtonEvent );
+  }
+
+  toggleEditMenu()
+  {
+    this.editMenu=!this.editMenu;
+  }
+
+  onClickedOutside(e: Event) {
+    if(this.editMenu)
+    {
+      ++this.counter;
+      if(this.counter > 1)
+      {
+        this.editMenu=false;
+        this.counter=0;
+      }
+    }
   }
 
 }
