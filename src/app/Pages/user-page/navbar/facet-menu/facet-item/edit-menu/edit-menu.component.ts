5ebb47b45b73518ba1be15a1042b9ba556ce4217
Zain Lateef <zainlateef@Zains-MBP.hsd1.ca.comcast.net>
2018-07-29 15:09:12 -0700

    everything appears to work. Looks like refactoring is complete

---
+++ src/app/Pages/user-page/navbar/facet-menu/facet-item/edit-menu/edit-menu.component.ts
@@ -0,0 +1,143 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IconSearchResult } from '../../../../../../Models/icon-search-result';
+import { FormControl } from '../../../../../../../../node_modules/@angular/forms';
+import { FacetItem } from '../../../../../../Models/facet-item';
+import { EditButtonService } from '../../../../../../Services/edit-button.service';
+declare var $: any;
+
+@Component({
+  selector: 'edit-menu',
+  template:
+  `
+    <div class="editMenu" *ngIf="editMode" (clickOutside)="onClickedOutside($event)">
+      <img src="/assets/images/close.png">
+      <div class="fullEditMenu" *ngIf="showFullEditMenuValue">
+        <input class="iconSearchbar" placeholder="Search for an icon..." type="search" [formControl]="search">
+        <ul class="iconSearchResults">
+          <li *ngFor="let icon of iconMatches">
+            <div class="listWrapper">
+              <i [class]="icon.iconFullName"></i>
+              <div class="nickname">{{icon.iconNickname}}</div>
+            </div>
+          </li>
+        </ul>
+        <button class="colorpicker" (colorPickerOpen)="colorPickerOpened(color)" [(colorPicker)]="color" (colorPickerChange)="setColor(color)"[style.background]="color" [cpPosition]="colorPickerOrientation" [cpDisableInput]="true"></button>
+        <input class="labelInput" placeholder="Label" [value]="item.label">
+      </div>
+    </div>
+  `,
+  styleUrls: ['./edit-menu.component.scss','../../original_style.scss']
+})
+export class EditMenuComponent implements OnInit {
+  
+  @Input("item") item : FacetItem;
+
+  showFullEditMenuValue : boolean=false;
+
+  @Output() showFullEditMenuChange = new EventEmitter<boolean>();
+
+  @Input("showFullEditMenu")
+  get showFullEditMenu()
+  {
+    return this.showFullEditMenuValue;
+  }
+  set showFullEditMenu(val) {
+    this.showFullEditMenuValue = val;
+    this.showFullEditMenuChange.emit(this.showFullEditMenuValue);
+    console.log("set to:"+this.showFullEditMenu)
+  }
+
+  search : FormControl;
+  colorPickerOrientation : string = "bottom";
+  editMode : boolean = false;
+  color : any;
+  iconMatches : Set<IconSearchResult> = new Set;
+  iconDatabase : Array<IconSearchResult> = new Array;
+  subscription : any;
+  counter : number = 0;
+
+  constructor(private editService : EditButtonService) { }
+
+  ngOnInit() {
+    this.editServiceSetup();
+    this.initializeForm();
+    this.initializeDummyData();
+    this.color=this.item.color;
+  }
+  
+  editServiceSetup()
+  {
+    this.editMode=this.editService.editMode;
+    if(this.editMode)
+    this.showFullEditMenuValue=true;
+    this.editService.editValueChange.subscribe( editButtonEvent => this.editMode=editButtonEvent );
+  }
+
+  onFormChange(term: any) {
+    //HTTP call returns set of users. All this logic will be rewritten and optimized
+    //console.log("HTTP Call:Query this parameter"+term);
+    this.iconMatches.clear();
+    if(term!=="")
+    {
+      this.iconDatabase.filter( icon => {
+        if(icon.iconNickname.toUpperCase().includes(term.toUpperCase()))
+          this.iconMatches.add(icon);
+      });
+    }
+  }
+
+  colorPickerOpened(color)
+  {
+    let el=$( '.color-picker' );
+    let boundsOfColorPicker = el[0].getBoundingClientRect();
+    //console.log(this.isElementInViewport(boundsOfColorPicker));
+    if(!this.isElementInViewport(boundsOfColorPicker))
+      this.colorPickerOrientation="top";
+    //reset the color picker
+  }
+
+  isElementInViewport(rectangle) 
+  {
+    return rectangle.top >= 0 && rectangle.left >= 0 && rectangle.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rectangle.right <= (window.innerWidth || document.documentElement.clientWidth);
+  }
+
+  setColor(color)
+  {
+    this.item.color=color;
+  }
+
+  onClickedOutside(e: Event) {
+    if(this.showFullEditMenu)
+    {
+      console.log("pass");
+      ++this.counter;
+      if(this.counter > 1)
+      {
+        this.showFullEditMenu=false;
+        this.counter=0;
+      }
+    }
+    else
+    this.counter=0;
+  }
+
+  initializeForm() 
+  {
+    this.search=new FormControl();
+    this.subscription=this.search.valueChanges
+        .debounceTime(200)
+        .distinctUntilChanged()
+        .subscribe( term => {
+            this.onFormChange(term);
+        });
+  }
+
+  initializeDummyData()
+  {
+    let icon1=new IconSearchResult("cog","fas fa-cog")
+    let icon2=new IconSearchResult("plane","fas fa-plane")
+    let icon3=new IconSearchResult("archive","fas fa-archive")
+    this.iconDatabase.push(icon1,icon2,icon3);
+  }
+
+}
--- src/app/Pages/user-page/navbar/facet-menu/facet-item/facet-item.component.ts
+++ src/app/Pages/user-page/navbar/facet-menu/facet-item/facet-item.component.ts
@@ -1,38 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FacetItem } from '../../../../../Models/facet-item';
 import { EditButtonService } from '../../../../../Services/edit-button.service';
-import { FormControl } from '@angular/forms';
-import { IconSearchResult } from '../../../../../Models/icon-search-result';
 import { Router, ActivatedRoute } from '../../../../../../../node_modules/@angular/router';
-declare var $: any;
 
 @Component({
   selector: 'facet-item',
   template: 
-  ` <a (click)="routeToFacet()" class="nav__link">
+  ` <a (click)="routeToFacet();toggleFullEditMenu($event)" class="nav__link">
       <div class="wrapper">
-
-        <div class="icon_wrapper" (click)="toggleEditExtraOptions($event)">
+        <div class="icon_wrapper">
           <i [ngStyle]="{'color':item.color}" [class]="item.iconName" [ngClass]="{'faa-float animated faa-fast': editMode}" [matTooltip]="!editMode ? item.label : null" [matTooltipShowDelay]="400"></i>
         </div>
-
-        <div class="editMenu" *ngIf="editMode" (clickOutside)="onClickedOutside($event)">
-          <img src="/assets/images/close.png">
-          <div class="editExtraOptions" *ngIf="editExtraOptions">
-            <input class="iconSearchbar" placeholder="Search for an icon..." type="search" [formControl]="search">
-            <ul class="iconSearchResults">
-              <li *ngFor="let icon of iconMatches">
-                <div class="listWrapper">
-                  <i [class]="icon.iconFullName"></i>
-                  <div class="nickname">{{icon.iconNickname}}</div>
-                </div>
-              </li>
-            </ul>
-            <button class="colorpicker" (colorPickerOpen)="colorPickerOpened(color)" [(colorPicker)]="color" (colorPickerChange)="setColor(color)"[style.background]="color" [cpPosition]="colorPickerOrientation" [cpDisableInput]="true"></button>
-            <input class="tooltipInput" placeholder="Label" [value]="item.label">
-          </div>
-        </div>
-
+        <edit-menu [item]="item" [(showFullEditMenu)]="showFullEditMenu"></edit-menu>
       </div>
     </a>
   `
@@ -41,16 +20,9 @@ declare var $: any;
 })
 export class FacetItemComponent implements OnInit {
   @Input("item") item : FacetItem;
-  search : FormControl;
-  colorPickerOrientation : string = "bottom";
-  editMode : boolean = false;
-  editExtraOptions : boolean = false;
-  counter : number = 0;
-  color : any;
-  subscription : any;
-  iconMatches : Set<IconSearchResult> = new Set;
-  iconDatabase : Array<IconSearchResult> = new Array;
-  
+  editMode : boolean;
+  showFullEditMenu : boolean = false;
+
   constructor(
     private editService : EditButtonService, 
     private router: Router, 
@@ -59,28 +31,6 @@ export class FacetItemComponent implements OnInit {
 
   ngOnInit() {
     this.editServiceSetup()
-    this.initializeForm();
-    this.initializeDummyData();
-    this.color=this.item.color;
-  }
-
-  initializeForm() 
-  {
-    this.search=new FormControl();
-    this.subscription=this.search.valueChanges
-        .debounceTime(200)
-        .distinctUntilChanged()
-        .subscribe( term => {
-            this.onFormChange(term);
-        });
-  }
-
-  initializeDummyData()
-  {
-    let icon1=new IconSearchResult("cog","fas fa-cog")
-    let icon2=new IconSearchResult("plane","fas fa-plane")
-    let icon3=new IconSearchResult("archive","fas fa-archive")
-    this.iconDatabase.push(icon1,icon2,icon3);
   }
 
   routeToFacet()
@@ -89,68 +39,21 @@ export class FacetItemComponent implements OnInit {
     this.router.navigate([{facet : this.item.label}], { relativeTo: this.route });
   }
 
-  onFormChange(term: any) {
-    //HTTP call returns set of users. All this logic will be rewritten and optimized
-    //console.log("HTTP Call:Query this parameter"+term);
-    this.iconMatches.clear();
-    if(term!=="")
-    {
-      this.iconDatabase.filter( icon => {
-        if(icon.iconNickname.toUpperCase().includes(term.toUpperCase()))
-          this.iconMatches.add(icon);
-      });
-    }
-  }
-
   editServiceSetup()
   {
     this.editMode=this.editService.editMode;
-    if(this.editMode)
-    this.editExtraOptions=true;
     this.editService.editValueChange.subscribe( editButtonEvent => this.editMode=editButtonEvent );
   }
 
-  toggleEditExtraOptions(mouseclick : MouseEvent)
+  toggleFullEditMenu(mouseclick : MouseEvent)
   {
+    console.log("Clicked");
     if(this.editMode)
     {
-      //console.log("this mouseclick: "+mouseclick.clientX+" "+mouseclick.clientY);
-      this.editExtraOptions=!this.editExtraOptions;
+      this.showFullEditMenu=true;
     }
     else
-    this.editExtraOptions=false;
-  }
-
-  onClickedOutside(e: Event) {
-    if(this.editExtraOptions)
-    {
-      ++this.counter;
-      if(this.counter > 1)
-      {
-        this.editExtraOptions=false;
-        this.counter=0;
-      }
-    }
-  }
-
-  colorPickerOpened(color)
-  {
-    let el=$( '.color-picker' );
-    let boundsOfColorPicker = el[0].getBoundingClientRect();
-    //console.log(this.isElementInViewport(boundsOfColorPicker));
-    if(!this.isElementInViewport(boundsOfColorPicker))
-      this.colorPickerOrientation="top";
-    //reset the color picker
-  }
-
-  isElementInViewport(rectangle) 
-  {
-    return rectangle.top >= 0 && rectangle.left >= 0 && rectangle.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rectangle.right <= (window.innerWidth || document.documentElement.clientWidth);
-  }
-
-  setColor(color)
-  {
-    this.item.color=color;
+    this.showFullEditMenu=false;
   }
 
 }
