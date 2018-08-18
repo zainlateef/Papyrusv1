4d41d00596d34d06e3f2b901bff6243010344444
Zain Lateef <zlateefwps@gmail.com>
2018-08-18 15:26:01 -0700

    added zoom in feature

--- src/app/Pages/user-page/profile-pic/profile-pic.component.ts
+++ src/app/Pages/user-page/profile-pic/profile-pic.component.ts
@@ -8,23 +8,23 @@ import { zoomIn } from '../../../../../node_modules/ng-animate';
   template: `
   <div class="wrapper">
     <img src="https://imagejournal.org/wp-content/uploads/bb-plugin/cache/23466317216_b99485ba14_o-panorama.jpg" alt="">
-    <div class="editIconProfPic">
+    <div class="editIconProfPic" *ngIf="pageOwner" [@zoomIn]="zoomIn">
       <span class="fa-stack fa-lg">
         <i class="fa fa-circle fa-stack-2x icon-background"></i>
-        <i name="editButton" *ngIf="pageOwner" class="fas fa-edit fa-stack-1x icon" [matTooltip]="'Change your profile pic'"  [matTooltipShowDelay]="400" [@zoomIn]="zoomIn"></i>
+        <i name="editButton" class="fas fa-edit fa-stack-1x icon" [matTooltip]="'Change your profile pic'"  [matTooltipShowDelay]="400"></i>
       </span>
     </div>
-    <div class="editIconBackground">
+    <div class="editIconBackground" *ngIf="pageOwner" [@zoomIn]="zoomIn">
     <span class="fa-stack fa-lg">
       <i class="fa fa-circle fa-stack-2x icon-background"></i>
-      <i name="editButton" *ngIf="pageOwner" class="fas fa-edit fa-stack-1x icon" [matTooltip]="'Change your background pic'"  [matTooltipShowDelay]="400" [@zoomIn]="zoomIn"></i>
+      <i name="editButton" class="fas fa-edit fa-stack-1x icon" [matTooltip]="'Change your background pic'"  [matTooltipShowDelay]="400"></i>
     </span>
     </div>
   </div>        
   `,
   styleUrls: ['./profile-pic.component.scss'],
   animations: [
-    trigger('zoomIn', [transition('void => *', useAnimation(zoomIn, { params:{timing:0.15} } ))])
+    trigger('zoomIn', [transition('void => *', useAnimation(zoomIn, { params:{timing:0.3} } ))])
   ]
 })
 export class ProfilePicComponent implements OnInit {
