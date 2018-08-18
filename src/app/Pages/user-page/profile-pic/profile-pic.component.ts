5a7c7a7efe80baa879c3b36db782e5f14d120f7d
Zain Lateef <zlateefwps@gmail.com>
2018-08-18 11:12:41 -0700

    pausing production to fix devops issue

--- src/app/Pages/user-page/profile-pic/profile-pic.component.ts
+++ src/app/Pages/user-page/profile-pic/profile-pic.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { EditButtonService } from '../../../Services/edit-button.service';
 
 @Component({
   selector: 'profile-pic',
-  template: '<img src="https://imagejournal.org/wp-content/uploads/bb-plugin/cache/23466317216_b99485ba14_o-panorama.jpg" alt="">',
+  template: `
+  <div class="wrapper">
+    <img src="https://imagejournal.org/wp-content/uploads/bb-plugin/cache/23466317216_b99485ba14_o-panorama.jpg" alt="">
+    <i name="editButton" *ngIf="pageOwner" class="fas fa-edit editIcon" (click)="toggleEditMode()" [matTooltip]="'Change your profile pic'"  [matTooltipShowDelay]="1300" [@zoomIn]="zoomIn"></i>
+  </div>        
+  `,
   styleUrls: ['./profile-pic.component.scss']
 })
 export class ProfilePicComponent implements OnInit {
 
-  constructor() { }
+  constructor(private editService : EditButtonService) { }
 
-  ngOnInit() {
+  pageOwner : boolean = false;
+
+  ngOnInit() 
+  {
+    this.editServiceSetup();
+  }
+
+  editServiceSetup()
+  {
+    this.pageOwner=this.editService.isPageOwner;
+    this.editService.pageOwnerStatus.subscribe( pageOwnerStatus => {
+      this.pageOwner=pageOwnerStatus;
+    });
   }
 
 }
