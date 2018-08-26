4f74a2549692e8f9b2563cb538d61284e5e1b48a
Zain Lateef <zlateefwps@gmail.com>
2018-08-26 13:21:03 -0700

    still has a bug where if you backspace you'll nix the font

--- src/app/Pages/user-page/feed/post/post.component.ts
+++ src/app/Pages/user-page/feed/post/post.component.ts
@@ -13,38 +13,25 @@ export class PostComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // var quill = new Quill('#editor', {
-    //   theme: 'snow',
-    //   modules: {
-    //   toolbar : [
-    //     { 'size': ['small', false, 'large','huge'] },
-    //     [{ 'font': [] }],
-    //     'bold', 'italic', 'underline',
-    //     { 'color': [] }, 
-    //     'link',         
-    //     {'list': 'ordered'}, { 'list': 'bullet' },
-    //     { 'align': [] }                        
-    //   ]
-    // }   
-    // });
-    var fonts = ['sofia', 'roboto'];
-    var Font = Quill.import('formats/font');
+    let fonts = ['arial', 'sofia','indie_flower'];
+    let Font = Quill.import('formats/font');
     Font.whitelist = fonts;
     Quill.register(Font, true);
-    
-    var quill = new Quill('#editor', {
+
+    let quill = new Quill('#editor', {
       modules: {
         toolbar: [
-          [{ header: [1, 2, false] }],
-          [{ direction: "rtl" }, { align: [] }],
-          [{font: ['sofia', 'roboto']}]
+          [{ 'size': ['small', false, 'large', 'huge'] }], 
+          [{ 'font': fonts }],
+          [{ 'color': [] }],  
+          ['bold', 'italic', 'underline'],
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{ 'align': [] }],
         ]
       },
       theme: 'snow'  // or 'bubble'
     });
-    
-    quill.format('font', 'sofia');
-
+    quill.format('font', 'arial');
   }
 
 }
